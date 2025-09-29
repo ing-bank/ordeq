abe355d58bcaa7701074b0fee0649b2baf705fb2
Neerhoff <nielsneerhoff@gmail.com>
2025-09-29 14:33:07 +0200

    Added resource for reassigning IO

---
+++ packages/ordeq-manifest/tests/examples/project/nodes_import_reassign.py
@@ -0,0 +1,20 @@
+from examples.project.catalog_1 import a, b
+from examples.project.catalog_2 import f
+from ordeq import node
+
+A = a
+B = b
+
+
+@node(inputs=[A, B], outputs=f)
+def func_a(a_val: str, b_val: str) -> str:
+    return a_val + b_val
+
+
+AA = a
+BB = b
+
+
+@node(inputs=[AA, BB], outputs=f)
+def func_b(a_val: str, b_val: str) -> str:
+    return a_val + b_val

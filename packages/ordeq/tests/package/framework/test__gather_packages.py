fe7101b9b529b85e675fcd882f3d3593cc234288
Simon Brugman <sfbbrugman@gmail.com>
2025-10-07 23:25:27 +0200

    update tests

--- packages/ordeq/tests/package/framework/test__gather_packages.py
+++ packages/ordeq/tests/package/framework/test__gather_packages.py
@@ -1,13 +1,37 @@
 import importlib
 
 import pytest
-from ordeq.framework._gather import _collect_nodes_and_ios
+from ordeq.framework._gather import (
+    _resolve_runnables_to_nodes,
+    _resolve_runnables_to_nodes_and_ios,
+)
 
 
 @pytest.mark.parametrize(
     ("imports", "expected_nodes", "expected_ios"),
     [
-        pytest.param(["example"], [], [], id="example"),
+        pytest.param(
+            ["example"],
+            [
+                "example.nodes:world",
+                "example.pipeline:transform_input",
+                "example.pipeline:transform_mock_input",
+                "example.wrapped_io:hello",
+                "example.wrapped_io:print_message",
+            ],
+            [
+                "Hello:StringBuffer",
+                "TestInput:MockInput",
+                "TestOutput:MockOutput",
+                "World:StringBuffer",
+                "message:SayHello",
+                "name_generator:NameGenerator",
+                "name_printer:NamePrinter",
+                "x:StringBuffer",
+                "y:StringBuffer",
+            ],
+            id="example",
+        ),
         pytest.param(
             [
                 "example",
@@ -18,10 +42,16 @@ from ordeq.framework._gather import _collect_nodes_and_ios
             ],
             [
                 "example.nodes:world",
+                "example.pipeline:transform_input",
+                "example.pipeline:transform_mock_input",
                 "example.wrapped_io:hello",
                 "example.wrapped_io:print_message",
             ],
             [
+                "Hello:StringBuffer",
+                "TestInput:MockInput",
+                "TestOutput:MockOutput",
+                "World:StringBuffer",
                 "message:SayHello",
                 "name_generator:NameGenerator",
                 "name_printer:NamePrinter",
@@ -40,11 +70,43 @@ from ordeq.framework._gather import _collect_nodes_and_ios
             ],
             id="example_wrapped_io",
         ),
-        pytest.param(["example2"], [], [], id="example2"),
-        pytest.param(["example3"], [], [], id="example3"),
-        pytest.param(["duplicates"], [], [], id="duplicates"),
+        pytest.param(
+            ["example2"],
+            ["example2.nodes:transform_input_2"],
+            ["TestInput2:Input", "TestOutput2:Output"],
+            id="example2",
+        ),
+        pytest.param(
+            ["example3"],
+            ["example3.func_defs:hello", "example3.func_defs:hello"],
+            [],
+            id="example3",
+        ),
+        pytest.param(
+            ["duplicates"],
+            ["duplicates.file1:foo", "duplicates.file2:foo"],
+            ["x_value:Literal", "y_value:IO"],
+            id="duplicates",
+        ),
         pytest.param(["nested"], [], [], id="nested"),
-        pytest.param(["function_reuse"], [], [], id="function_reuse"),
+        pytest.param(
+            ["function_reuse"],
+            [
+                "function_reuse.func_defs:print_input",
+                "function_reuse.func_defs:print_input",
+                "function_reuse.func_defs:print_input",
+                "function_reuse.func_defs:print_input",
+                "function_reuse.nodes:pi",
+            ],
+            [
+                "A:StringBuffer",
+                "B:StringBuffer",
+                "C:StringBuffer",
+                "D:StringBuffer",
+                "another_name:StringBuffer",
+            ],
+            id="function_reuse",
+        ),
         pytest.param(
             ["function_reuse.nodes"],
             [
@@ -57,13 +119,69 @@ from ordeq.framework._gather import _collect_nodes_and_ios
             ["A:StringBuffer", "B:StringBuffer"],
             id="function_reuse_nodes_only",
         ),
-        pytest.param(["rag_pipeline"], [], [], id="rag_pipeline"),
         pytest.param(
-            ["rag_pipeline", "rag_pipeline.rag"], [], [], id="rag_pipeline+rag"
+            ["rag_pipeline"],
+            [
+                "rag_pipeline.rag.annotation:annotate_documents",
+                "rag_pipeline.rag.evaluation:evaluate_answers",
+                "rag_pipeline.rag.indexer:create_vector_index",
+                "rag_pipeline.rag.policies:generate_questions",
+                "rag_pipeline.rag.question_answering:question_answering",
+                "rag_pipeline.rag.retrieval:filter_relevant",
+                "rag_pipeline.rag.retrieval:retrieve",
+            ],
+            [
+                "index:IO",
+                "llm_answers:IO",
+                "llm_model:IO",
+                "llm_vision_retrieval_model:IO",
+                "metrics:IO",
+                "pdf_documents:IO",
+                "pdfs_documents_annotated:IO",
+                "policies:IO",
+                "questions:IO",
+                "relevant_pages:IO",
+                "retrieved_pages:IO",
+            ],
+            id="rag_pipeline",
+        ),
+        pytest.param(
+            ["rag_pipeline", "rag_pipeline.rag"],
+            [
+                "rag_pipeline.rag.annotation:annotate_documents",
+                "rag_pipeline.rag.evaluation:evaluate_answers",
+                "rag_pipeline.rag.indexer:create_vector_index",
+                "rag_pipeline.rag.policies:generate_questions",
+                "rag_pipeline.rag.question_answering:question_answering",
+                "rag_pipeline.rag.retrieval:filter_relevant",
+                "rag_pipeline.rag.retrieval:retrieve",
+            ],
+            [
+                "index:IO",
+                "llm_answers:IO",
+                "llm_model:IO",
+                "llm_vision_retrieval_model:IO",
+                "metrics:IO",
+                "pdf_documents:IO",
+                "pdfs_documents_annotated:IO",
+                "policies:IO",
+                "questions:IO",
+                "relevant_pages:IO",
+                "retrieved_pages:IO",
+            ],
+            id="rag_pipeline+rag",
         ),
         pytest.param(
             ["rag_pipeline.rag", "rag_pipeline.catalog"],
-            [],
+            [
+                "rag_pipeline.rag.annotation:annotate_documents",
+                "rag_pipeline.rag.evaluation:evaluate_answers",
+                "rag_pipeline.rag.indexer:create_vector_index",
+                "rag_pipeline.rag.policies:generate_questions",
+                "rag_pipeline.rag.question_answering:question_answering",
+                "rag_pipeline.rag.retrieval:filter_relevant",
+                "rag_pipeline.rag.retrieval:retrieve",
+            ],
             [
                 "index:IO",
                 "llm_answers:IO",
@@ -79,7 +197,20 @@ from ordeq.framework._gather import _collect_nodes_and_ios
             ],
             id="rag_rag+catalog",
         ),
-        pytest.param(["rag_pipeline.rag"], [], [], id="rag_rag"),
+        pytest.param(
+            ["rag_pipeline.rag"],
+            [
+                "rag_pipeline.rag.annotation:annotate_documents",
+                "rag_pipeline.rag.evaluation:evaluate_answers",
+                "rag_pipeline.rag.indexer:create_vector_index",
+                "rag_pipeline.rag.policies:generate_questions",
+                "rag_pipeline.rag.question_answering:question_answering",
+                "rag_pipeline.rag.retrieval:filter_relevant",
+                "rag_pipeline.rag.retrieval:retrieve",
+            ],
+            [],
+            id="rag_rag",
+        ),
         pytest.param(
             [
                 "rag_pipeline.rag.annotation",
@@ -149,7 +280,8 @@ def test_gather_nodes_and_ios_from_package(
         importlib.import_module(import_path) for import_path in imports
     ]
 
-    nodes, ios = _collect_nodes_and_ios(*runnables)
+    nodes, ios = _resolve_runnables_to_nodes_and_ios(*runnables)
+    assert nodes == _resolve_runnables_to_nodes(*runnables)
     assert expected_nodes == sorted(n.name for n in nodes)
     assert expected_ios == sorted([
         f"{name}:{type(io).__name__}" for name, io in ios.items()

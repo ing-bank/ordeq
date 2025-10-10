75003b21d6576712059966d6841ec58d1789af32
Neerhoff <nielsneerhoff@gmail.com>
2025-10-10 10:51:09 +0200

    Formatting

--- packages/ordeq-test-utils/tests/test_package.py
+++ packages/ordeq-test-utils/tests/test_package.py
@@ -1,2 +1,4 @@
 def test_discovery():
-    from ordeq_test_utils import compare_resources_against_snapshots
+    from ordeq_test_utils import (
+        compare_resources_against_snapshots,  # noqa: F401 (imported but unused)
+    )

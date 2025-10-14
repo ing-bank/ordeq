6d6e20432e91ae52702d683593ee4b54a75dfd00
Neerhoff <nielsneerhoff@gmail.com>
2025-10-14 17:21:05 +0200

    Rm pandas

--- packages/ordeq-pandas/src/ordeq_pandas/__init__.py
+++ packages/ordeq-pandas/src/ordeq_pandas/__init__.py
@@ -1,6 +1,5 @@
 from ordeq_pandas.csv import PandasCSV
-from ordeq_pandas.dataframe import PandasDataFrame
 from ordeq_pandas.excel import PandasExcel
 from ordeq_pandas.parquet import PandasParquet
 
-__all__ = ("PandasCSV", "PandasDataFrame", "PandasExcel", "PandasParquet")
+__all__ = ("PandasCSV", "PandasExcel", "PandasParquet")

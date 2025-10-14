4a62cd26864bb34f1f0ec8b85ecfab19364ed4b1
Neerhoff <nielsneerhoff@gmail.com>
2025-10-14 07:41:55 +0200

    Resources

--- packages/ordeq/tests/resources/nodes/views/view_dataframe_cast.py
+++ packages/ordeq/tests/resources/nodes/views/view_dataframe_cast.py
@@ -2,24 +2,26 @@ from ordeq import node
 from ordeq._nodes import get_node
 import pandas as pd
 from ordeq_common import Print
+from ordeq_pandas import PandasDataFrame
 
-DataFrame = pd.DataFrame(
-    {
-        "A": [1, 2, 3],
-        "B": [4.0, 5.5, 6.1],
-        "C": ["foo", "bar", "baz"],
-        "D": [True, False, True],
-    }
+DataFrame = PandasDataFrame(
+    data=(
+        (1, 2, 3),
+        (4.0, 5.5, 6.1),
+        ("foo", "bar", "baz"),
+        (True, False, True),
+    ),
+    columns=("A", "B", "C", "D"),
 )
 
 
 @node(inputs=DataFrame)
-def view(df: pd.DataFrame) -> pd.DataFrame:
+def df_casted(df: pd.DataFrame) -> pd.DataFrame:
     df["A"] = df["A"].astype("string")
     return df
 
 
-@node(inputs=view, outputs=Print())
+@node(inputs=df_casted, outputs=Print())
 def group_by(df: pd.DataFrame) -> pd.DataFrame:
     return df.groupby(
         by=["A", ],

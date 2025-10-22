805bb4d934b42c9c071383070f8c86844d863c84
josersanvil <josersanvil@gmail.com>
2025-10-22 16:26:26 +0200

    Remove unnecesary data folder from integration-marimo

--- docs/guides/integrations/marimo.md
+++ docs/guides/integrations/marimo.md
@@ -1,7 +1,13 @@
-# Marimo
+# Using Ordeq in a Marimo notebook
 
 [Marimo](https://docs.marimo.io/) is an open-source reactive Python notebook. An improved version of Jupyter notebooks, Marimo allows you to build data applications with interactive widgets and reactive cells.
 
-Ordeq integrates seamlessly with Marimo, enabling you to create interactive data applications that leverage Ordeq's capabilities.
+This example demonstrates how to use Ordeq within a Marimo notebook to create an interactive data application.
 
-You can find an example of using Ordeq with Marimo in the [repository's examples section](https://github.com/ing-bank/ordeq/tree/main/examples)
+Even though this example is written for Marimo, you can run similar code in a Jupyter notebook as well to run Ordeq in the interactive environment of your choice.
+
+/// marimo-embed-file
+    size: medium
+    mode: edit
+    filepath: examples/marimo_ordeq/src/marimo_example.py
+///
--- examples/integration-marimo/src/marimo_example.py
+++ examples/integration-marimo/src/marimo_example.py
@@ -7,7 +7,6 @@ app = marimo.App(width="medium")
 @app.cell
 def _():
     import marimo as mo
-
     return (mo,)
 
 
@@ -20,6 +19,7 @@ def _(mo):
     Ordeq is a framework for developing data pipelines. It simplifies IO and modularizes pipeline logic.
     """
     )
+    return
 
 
 @app.cell
@@ -33,16 +33,17 @@ def _(mo):
     Typically you'd put them in a file called `catalog.py`
     """
     )
+    return
 
 
 @app.cell
 def _():
     # catalog.py
+    from ordeq_polars import PolarsEagerCSV
     from pathlib import Path
 
-    from ordeq_polars import PolarsEagerCSV
-
     data_dir = Path(__file__).parent / "data"
+    data_dir.mkdir(exist_ok=True)
 
     # Input data
     user_data = PolarsEagerCSV(path=data_dir / "users.csv")
@@ -54,10 +55,43 @@ def _():
 
 
 @app.cell
+def _(mo):
+    mo.md(r"""Let's first create a Polars DataFrame containing our user data, then use the `PolarsEagerCSV` IO to write it to a CSV file.""")
+    return
+
+
+@app.cell
+def _():
+    import polars as pl
+
+    user_data_df = pl.DataFrame(
+        [
+            {"Name":"John Doe","Email":"john@example.com","Phone":"555-123-4567"},
+            {"Name":"Jane Smith","Email":"jane@example.com","Phone":"555-987-6543"},
+            {"Name":"Peter Jones","Email":"peter@sample.com","Phone":"555-555-1212"},
+            {"Name":"Rachel Adams","Email":"rachel.adams@test.com","Phone":"555-444-3333"},
+            {"Name":"Emily Davis","Email":"emily.davis@sample.com","Phone":"555-333-2222"},
+            {"Name":"Michael Brown","Email":"michael.brown@example2.com","Phone":"555-222-1111"},
+            {"Name":"Jane L. Smith","Email":"jane@example.com","Phone":"555-987-6543"}
+        ]
+    )
+    user_data_df
+    return pl, user_data_df
+
+
+@app.cell
+def _(user_data, user_data_df):
+    # Write the data to a file using the IO object's .save() method:
+    user_data.save(user_data_df)
+    return
+
+
+@app.cell
 def _(user_data):
-    # Inspect the data from the IO objects using .load()
+    # You can also load the data from using the IO object's .load() method:
 
     user_data.load()
+    return
 
 
 @app.cell
@@ -66,17 +100,17 @@ def _(mo):
         r"""
     **Nodes** are Python functions decorated with `@node`, which implement the business logic of your pipeline.
 
-    Ordeq automatically loads and passes the `IO` objects that you mark as `inputs` of the node to the function and saves the data returned by the function to the `IO` objects marked as `outputs`
+    Ordeq automatically loads and passes the `IO` objects that you mark as `inputs` of the node to the function and saves the data returned by the function to the `IO` objects marked as `outputs`. This way you don't need to worry about the inputs and outputs of your transformations and only focus on the business logic of your application.
     """
     )
+    return
 
 
 @app.cell
-def _(clean_users_data, user_data, user_metrics):
+def _(clean_users_data, pl, user_data, user_metrics):
     # nodes.py
-    import polars as pl
-    from ordeq import node
     from polars import DataFrame
+    from ordeq import node
 
     @node(inputs=[user_data], outputs=[clean_users_data])
     def clean_users(user_data_df: DataFrame) -> DataFrame:
@@ -95,15 +129,13 @@ def _(clean_users_data, user_data, user_metrics):
             pl.col("email_domain").n_unique().alias("unique_email_domains"),
             pl.col("phone").n_unique().alias("unique_phone_numbers"),
         )
-
     return clean_users, extract_user_metrics
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        r"""You can visualize the pipeline you've built by using the `ordeq-viz` package"""
-    )
+    mo.md(r"""You can visualize the pipeline you've built by using the `ordeq-viz` package""")
+    return
 
 
 @app.cell
@@ -112,13 +144,13 @@ def _(clean_users, extract_user_metrics, mo):
 
     diagram = viz(clean_users, extract_user_metrics, fmt="mermaid")
     mo.mermaid(diagram)
+    return
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        r"""Then run the pipeline by using `ordeq.run`, which takes the same arguments as `viz`"""
-    )
+    mo.md(r"""Then run the pipeline by using `ordeq.run`, which takes the same arguments as `viz`""")
+    return
 
 
 @app.cell
@@ -126,21 +158,25 @@ def _(clean_users, extract_user_metrics):
     from ordeq import run
 
     run(clean_users, extract_user_metrics)
+    return
 
 
 @app.cell
 def _(mo):
     mo.md(r"""Inspect the saved outputs""")
+    return
 
 
 @app.cell
 def _(clean_users_data):
     clean_users_data.load()
+    return
 
 
 @app.cell
 def _(user_metrics):
     user_metrics.load()
+    return
 
 
 @app.cell

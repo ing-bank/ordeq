10387d9476510c7f01597fd6013a9e770e407c55
Neerhoff <nielsneerhoff@gmail.com>
2025-10-24 09:28:09 +0200

    Polish error msg

--- packages/ordeq/src/ordeq/_nodes.py
+++ packages/ordeq/src/ordeq/_nodes.py
@@ -142,7 +142,7 @@ def _raise_for_invalid_outputs(n: Node) -> None:
     if not all(are_outputs):
         not_an_output = n.outputs[are_outputs.index(False)]
         raise ValueError(
-            f"Outputs of node {n.name} must be of type Output, "
+            f"Outputs of node '{n.name}' must be of type Output, "
             f"found {type(not_an_output)} "
         )
 

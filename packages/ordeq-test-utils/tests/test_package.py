4cf8c9880cb539aca14946692bd27abf17567775
Neerhoff <nielsneerhoff@gmail.com>
2025-10-24 12:58:45 +0200

    Regenerated snapshots in Markdown format

--- packages/ordeq-test-utils/tests/test_package.py
+++ packages/ordeq-test-utils/tests/test_package.py
@@ -1,11 +1,4 @@
-from pathlib import Path
-from ordeq_test_utils.snapshot import capture_module
-
-
-def test_it_captures_module(
-    caplog, capsys
-):
-    capture_module(
-        Path(__file__).resolve().parent / "expr.py",
-                   caplog,
-                   capsys)
+def test_discovery():
+    from ordeq_test_utils import (
+        compare_resources_against_snapshots,  # noqa: F401 (imported but unused)
+    )

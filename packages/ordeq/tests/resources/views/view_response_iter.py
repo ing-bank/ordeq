9e8ccf9d7b3d2b0f24a20f1fdf15a47d43b21ad6
Neerhoff <nielsneerhoff@gmail.com>
2025-10-29 17:59:07 +0100

    Reverted to requests in tests

--- packages/ordeq/tests/resources/views/view_response_iter.py
+++ packages/ordeq/tests/resources/views/view_response_iter.py
@@ -1,31 +1,11 @@
-from collections.abc import Iterator
-from typing import Any
-
 import requests
+from typing import Any, Iterator
+
 from ordeq import node, run
 from ordeq_common import Literal
 
-# Data retrieved from https://jsonplaceholder.typicode.com/users/1
-users_response = Literal({
-    "id": 1,
-    "name": "Leanne Graham",
-    "username": "Bret",
-    "email": "Sincere@april.biz",
-    "address": {
-        "street": "Kulas Light",
-        "suite": "Apt. 556",
-        "city": "Gwenborough",
-        "zipcode": "92998-3874",
-        "geo": {"lat": "-37.3159", "lng": "81.1496"},
-    },
-    "phone": "1-770-736-8031 x56442",
-    "website": "hildegard.org",
-    "company": {
-        "name": "Romaguera-Crona",
-        "catchPhrase": "Multi-layered client-server neural-net",
-        "bs": "harness real-time e-markets",
-    },
-})
+response = requests.get("https://jsonplaceholder.typicode.com/users/1")
+users_response = Literal(response)
 
 
 # View that returns an iterable from a regular/non-iterable IO:
--- packages/ordeq/tests/resources/views/view_response_stream.py
+++ packages/ordeq/tests/resources/views/view_response_stream.py
@@ -1,30 +1,11 @@
-from collections.abc import Generator
-
 import requests
-from ordeq import node, run
-from ordeq_common import Literal, Print
 
-# Data retrieved from https://jsonplaceholder.typicode.com/users/1
-users_response = Literal({
-    "id": 1,
-    "name": "Leanne Graham",
-    "username": "Bret",
-    "email": "Sincere@april.biz",
-    "address": {
-        "street": "Kulas Light",
-        "suite": "Apt. 556",
-        "city": "Gwenborough",
-        "zipcode": "92998-3874",
-        "geo": {"lat": "-37.3159", "lng": "81.1496"},
-    },
-    "phone": "1-770-736-8031 x56442",
-    "website": "hildegard.org",
-    "company": {
-        "name": "Romaguera-Crona",
-        "catchPhrase": "Multi-layered client-server neural-net",
-        "bs": "harness real-time e-markets",
-    },
-})
+from ordeq import node, run
+from ordeq_common import Print, Literal
+from typing import Generator
+
+response = requests.get("https://jsonplaceholder.typicode.com/users/1")
+users_response = Literal(response)
 
 
 @node(inputs=users_response)

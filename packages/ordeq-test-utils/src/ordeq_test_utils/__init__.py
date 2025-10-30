76f8375b4d4600f3ef714d07098ea98e8514b254
Neerhoff <nielsneerhoff@gmail.com>
2025-10-30 17:00:16 +0100

    Fmt

--- packages/ordeq-test-utils/src/ordeq_test_utils/__init__.py
+++ packages/ordeq-test-utils/src/ordeq_test_utils/__init__.py
@@ -1,4 +1,7 @@
 from ordeq_test_utils.snapshot import compare_resources_against_snapshots
 from ordeq_test_utils.typing import capture_type_check_and_compare
 
-__all__ = ("compare_resources_against_snapshots", "capture_type_check_and_compare")
+__all__ = (
+    "capture_type_check_and_compare",
+    "compare_resources_against_snapshots",
+)

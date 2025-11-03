d0db87c1294577a00768a302fb4c7678bae034df
josersanvil <josersanvil@gmail.com>
2025-11-03 19:29:27 +0100

    Replace Text with FileStream for package overview generation

--- packages/ordeq-dev-tools/src/ordeq_dev_tools/pipelines/docs_package_overview.py
+++ packages/ordeq-dev-tools/src/ordeq_dev_tools/pipelines/docs_package_overview.py
@@ -2,17 +2,17 @@
 directory. The resulting markdown is written to docs/packages.md.
 """
 
-import io
 from pathlib import Path
+from collections.abc import Iterator
 
 from ordeq import node
 from ordeq_dev_tools.paths import ROOT_PATH, PACKAGES_PATH
 from ordeq_dev_tools.pipelines.shared import packages
-from ordeq_files import Text
+from ordeq_files import FileStream
 from ordeq_toml import TOML
 
 
-package_overview = Text(path=ROOT_PATH / "docs" / "packages.md")
+package_overview = FileStream[str](path=ROOT_PATH / "docs" / "packages.md", mode="w+")
 
 
 def get_pypi_name_description_group_logo(
@@ -71,7 +71,7 @@ def groups(
 
 
 @node(inputs=groups, outputs=package_overview)
-def write_html_table_by_group(groups: dict[str, list[dict]]) -> str:
+def write_html_table_by_group(groups: dict[str, list[dict]]) -> Iterator[str]:
     """Write the grouped HTML tables to the given output_path, with logo column and proper sizing.
 
     Args:
@@ -99,19 +99,16 @@ def write_html_table_by_group(groups: dict[str, list[dict]]) -> str:
         "Other": "Packages not assigned to a specific group.",
     }
 
-    f = io.StringIO()
-    f.write(header)
+    yield header
     # Write groups in the specified order first
     for group in group_order:
         if group in groups and group not in excluded_groups:
             pretty = pretty_names.get(group, group)
             desc = group_descriptions.get(group, "")
-            f.write(f"## {pretty}\n\n")
+            yield f"## {pretty}\n\n"
             if desc:
-                f.write(f"{desc}\n\n")
-            f.write(
-                "<table>\n  <tr>\n    <th width='90' style='text-align:center;vertical-align:middle;max-height:35px;'></th>\n    <th style='text-align:left;vertical-align:middle;max-height:35px;'>Name</th>\n    <th style='text-align:left;vertical-align:middle;max-height:35px;'>Description</th>\n    <th style='text-align:left;vertical-align:middle;max-height:35px;'>API Docs</th>\n  </tr>\n"
-            )
+                yield f"{desc}\n\n"
+            yield "<table>\n  <tr>\n    <th width='90' style='text-align:center;vertical-align:middle;max-height:35px;'></th>\n    <th style='text-align:left;vertical-align:middle;max-height:35px;'>Name</th>\n    <th style='text-align:left;vertical-align:middle;max-height:35px;'>Description</th>\n    <th style='text-align:left;vertical-align:middle;max-height:35px;'>API Docs</th>\n  </tr>\n"
             for pkg in groups[group]:
                 logo_html = (
                     f"<img src='{pkg['logo_url']}' alt='logo' style='max-height:35px;'/>"
@@ -123,10 +120,8 @@ def write_html_table_by_group(groups: dict[str, list[dict]]) -> str:
                     f"<img src='https://img.shields.io/pypi/v/{pkg['pypi_name']}?label={pkg['pkg_dir']}' style='max-height:35px;'/></a>"
                 )
                 docs_html = f"<a href='https://ing-bank.github.io/ordeq/api/{pkg['src_name']}/'>API Docs</a>"
-                f.write(
-                    f"  <tr>\n    <td align='center' width='90' style='text-align:center;vertical-align:middle;max-height:35px;'>{logo_html}</td>\n    <td align='left' style='text-align:left;vertical-align:middle;max-height:35px;'>{name_html}</td>\n    <td align='left' style='text-align:left;vertical-align:middle;max-height:35px;'>{pkg['description']}</td>\n    <td align='left' style='text-align:left;vertical-align:middle;max-height:35px;'>{docs_html}</td>\n  </tr>\n"
-                )
-            f.write("</table>\n\n")
+                yield f"  <tr>\n    <td align='center' width='90' style='text-align:center;vertical-align:middle;max-height:35px;'>{logo_html}</td>\n    <td align='left' style='text-align:left;vertical-align:middle;max-height:35px;'>{name_html}</td>\n    <td align='left' style='text-align:left;vertical-align:middle;max-height:35px;'>{pkg['description']}</td>\n    <td align='left' style='text-align:left;vertical-align:middle;max-height:35px;'>{docs_html}</td>\n  </tr>\n"
+            yield "</table>\n\n"
 
     # Write any other groups (except excluded)
     for group in sorted(groups):
@@ -134,12 +129,10 @@ def write_html_table_by_group(groups: dict[str, list[dict]]) -> str:
             continue
         pretty = pretty_names.get(group, group)
         desc = group_descriptions.get(group, "")
-        f.write(f"## {pretty}\n\n")
+        yield f"## {pretty}\n\n"
         if desc:
-            f.write(f"{desc}\n\n")
-        f.write(
-            "<table>\n  <tr>\n    <th width='90' style='text-align:center;vertical-align:middle;max-height:35px;'></th>\n    <th style='text-align:left;vertical-align:middle;max-height:35px;'>Name</th>\n    <th style='text-align:left;vertical-align:middle;max-height:35px;'>Description</th>\n    <th style='text-align:left;vertical-align:middle;max-height:35px;'>API Docs</th>\n  </tr>\n"
-        )
+            yield f"{desc}\n\n"
+        yield "<table>\n  <tr>\n    <th width='90' style='text-align:center;vertical-align:middle;max-height:35px;'></th>\n    <th style='text-align:left;vertical-align:middle;max-height:35px;'>Name</th>\n    <th style='text-align:left;vertical-align:middle;max-height:35px;'>Description</th>\n    <th style='text-align:left;vertical-align:middle;max-height:35px;'>API Docs</th>\n  </tr>\n"
         for pkg in groups[group]:
             logo_html = (
                 f"<img src='{pkg['logo_url']}' alt='logo' style='max-height:35px;'/>"
@@ -151,8 +144,5 @@ def write_html_table_by_group(groups: dict[str, list[dict]]) -> str:
                 f"<img src='https://img.shields.io/pypi/v/{pkg['pypi_name']}?label={pkg['pkg_dir']}' style='max-height:35px;'/></a>"
             )
             docs_html = f"<a href='https://ing-bank.github.io/ordeq/api/{pkg['src_name']}/'>API Docs</a>"
-            f.write(
-                f"  <tr>\n    <td align='center' width='90' style='text-align:center;vertical-align:middle;max-height:35px;'>{logo_html}</td>\n    <td align='left' style='text-align:left;vertical-align:middle;max-height:35px;'>{name_html}</td>\n    <td align='left' style='text-align:left;vertical-align:middle;max-height:35px;'>{pkg['description']}</td>\n    <td align='left' style='text-align:left;vertical-align:middle;max-height:35px;'>{docs_html}</td>\n  </tr>\n"
-            )
-        f.write("</table>\n\n")
-    return f.getvalue()
+            yield f"  <tr>\n    <td align='center' width='90' style='text-align:center;vertical-align:middle;max-height:35px;'>{logo_html}</td>\n    <td align='left' style='text-align:left;vertical-align:middle;max-height:35px;'>{name_html}</td>\n    <td align='left' style='text-align:left;vertical-align:middle;max-height:35px;'>{pkg['description']}</td>\n    <td align='left' style='text-align:left;vertical-align:middle;max-height:35px;'>{docs_html}</td>\n  </tr>\n"
+        yield "</table>\n\n"

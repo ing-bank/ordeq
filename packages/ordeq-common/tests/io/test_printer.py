ff9b3e2e973918dde752d08e02ae20aa60d4ce12
Simon Brugman <sbrugman@users.noreply.github.com>
2025-11-06 15:29:24 +0100

    `ordeq-common`: allow save options in `Printer`, e.g. print to stderr (#283)

--- packages/ordeq-common/tests/io/test_printer.py
+++ packages/ordeq-common/tests/io/test_printer.py
@@ -1,3 +1,5 @@
+import sys
+
 from ordeq_common import Print
 
 
@@ -5,3 +7,11 @@ def test_it_saves(capsys):
     Print().save("Hello, world!")
     captured = capsys.readouterr()
     assert "Hello, world!" in captured.out
+    assert not captured.err
+
+
+def test_it_saves_stderr(capsys):
+    Print().save("Hello, world!", file=sys.stderr)
+    captured = capsys.readouterr()
+    assert not captured.out
+    assert "Hello, world!" in captured.err

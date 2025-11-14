61b04ccefa925f059b14c3e6b33a3e5dfa801504
Niels Neerhoff <nielsneerhoff@gmail.com>
2025-11-14 08:43:28 +0100

    Ruff

--- packages/ordeq/tests/package/test_resource.py
+++ packages/ordeq/tests/package/test_resource.py
@@ -1,11 +1,13 @@
 from ordeq._resource import Resource
 
+
 def test_resource_eq():
     a = Resource("path")
     b = Resource("path")
     assert a is not b
     assert a == b
     assert hash(a) == hash(b)
+
 
 def test_resource_neq():
     a = Resource("a")

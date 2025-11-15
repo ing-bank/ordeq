02a2415f40e76c7da3498853bdf438f133d63996
Niels Neerhoff <nielsneerhoff@gmail.com>
2025-11-15 19:56:31 +0100

    Fmt

--- packages/ordeq/tests/resources/graph/graph_ios_equal.py
+++ packages/ordeq/tests/resources/graph/graph_ios_equal.py
@@ -1,25 +1,25 @@
 # Resource showing that equivalent IO instances are modelled as distinct
 # vertices in the graph.
-from pprint import pprint
+
+import operator
 
 from ordeq import Input, Node
 from ordeq._graph import NodeIOGraph
 from ordeq_common import Print
-import operator
 
 
 class Greeting(Input[str]):
     def __init__(self, value: str):
         self.value = value
 
+    def load(self) -> str:
+        return self.value
+
     def __eq__(self, other):
         return self.value.lower() == self.value.lower()
 
     def __hash__(self):
         return hash(self.value.lower())
-
-    def load(self) -> str:
-        return self.value
 
 
 x = Greeting("hello")
@@ -28,7 +28,12 @@ assert hash(x) == hash(y)
 assert x == y
 
 graph = NodeIOGraph.from_nodes([
-    Node(func=operator.eq, inputs=(x, y), outputs=(Print(),), name="are_greetings_equal")
+    Node(
+        func=operator.eq,
+        inputs=(x, y),
+        outputs=(Print(),),
+        name="are_greetings_equal",
+    )
 ])
 print("NodeIOGraph:")
 print(graph)
--- packages/ordeq/tests/resources/graph/graph_ios_same_hash.py
+++ packages/ordeq/tests/resources/graph/graph_ios_same_hash.py
@@ -1,21 +1,22 @@
 # Resource showing that distinct IO objects with the same hash
 # are modelled as distinct vertices in the graph.
 
+import operator
+
 from ordeq import Input, Node
 from ordeq._graph import NodeIOGraph
 from ordeq_common import Print
-import operator
 
 
 class Greeting(Input[str]):
     def __init__(self, value: str):
         self.value = value
 
-    def __hash__(self):
-        return hash(self.value.lower())
-
     def load(self) -> str:
         return self.value
+
+    def __hash__(self):
+        return hash(self.value.lower())
 
 
 x = Greeting("hello")
@@ -23,7 +24,12 @@ y = Greeting("HELLO")
 assert hash(x) == hash(y)
 
 graph = NodeIOGraph.from_nodes([
-    Node(func=operator.eq, inputs=(x, y), outputs=(Print(),), name="are_greetings_equal")
+    Node(
+        func=operator.eq,
+        inputs=(x, y),
+        outputs=(Print(),),
+        name="are_greetings_equal",
+    )
 ])
 print("NodeIOGraph:")
 print(graph)

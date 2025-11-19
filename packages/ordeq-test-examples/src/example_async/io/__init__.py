4acc350e7bf0465064467ad3a5552d0dc3ece18c
Jose R. Sanchez Viloria <34132595+Josersanvil@users.noreply.github.com>
2025-11-19 14:49:00 +0100

    `ordeq`: Async IO examples (#378)

---
+++ packages/ordeq-test-examples/src/example_async/io/__init__.py
@@ -0,0 +1,4 @@
+from example_async.io.async_static_string import AsyncStaticString
+from example_async.io.async_string_buffer import AsyncStringBuffer
+
+__all__ = ["AsyncStaticString", "AsyncStringBuffer"]

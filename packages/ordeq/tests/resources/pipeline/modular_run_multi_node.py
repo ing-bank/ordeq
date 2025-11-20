0aa63e74d9614a28a31f5623d5609db9d1748f50
Simon Brugman <sfbbrugman@gmail.com>
2025-11-20 19:45:25 +0100

    basic testing

---
+++ packages/ordeq/tests/resources/pipeline/modular_run_multi_node.py
@@ -0,0 +1,30 @@
+# Users should be able to run runnables in idiomatic Python:
+# 1. running a pipeline should be as simple as calling a function
+# 2. args should be passed in-memory, not through IOs
+from typing import Any
+
+from ordeq import IO, node, pipeline
+
+x = IO[Any]()
+y = IO[Any]()
+
+
+@node(inputs=[x])
+def n1(a):
+    return a + 1
+
+
+@node(inputs=[n1, x])
+def n2(b, c):
+    return (b * 2) / c
+
+
+@node(inputs=[n2, x], outputs=y)
+def n3(c, d):
+    return int(d * c - 3)
+
+
+my_pipeline = pipeline(n1, n2, n3, inputs=[x], outputs=[y])
+
+output = my_pipeline(30)
+assert output == 59

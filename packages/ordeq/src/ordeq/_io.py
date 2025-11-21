be674f6047bd40af202b339fd8548ae27beb8fe7
Niels Neerhoff <40208446+nielsneerhoff@users.noreply.github.com>
2025-11-21 13:04:27 +0100

    `ordeq`: generic AnyIO (#402)

--- packages/ordeq/src/ordeq/_io.py
+++ packages/ordeq/src/ordeq/_io.py
@@ -665,7 +665,7 @@ class IO(Input[T], Output[T]):
 
 
 # Type aliases
-AnyIO: TypeAlias = Input | Output
+AnyIO: TypeAlias = Input[T] | Output[T]
 
 # Type alias for IO identity retrieved using id(). This is used to uniquely
 # identify IO instances. We cannot rely on the __eq__ and __hash__ of IO

70c6491d85860a1e2854ad790ec33fc470010e39
Simon Brugman <sfbbrugman@gmail.com>
2025-11-24 17:18:03 +0100

    `ordeq`: support staticmethod in IOs

--- packages/ordeq/src/ordeq/_io.py
+++ packages/ordeq/src/ordeq/_io.py
@@ -135,9 +135,6 @@ def _process_input_meta(name, bases, class_dict):
             )
             raise TypeError(msg)
 
-        if isinstance(load_method, staticmethod):
-            raise ValueError("Load method cannot be static.")
-
         # Ensure all arguments (except self/cls) have default values
         sig = inspect.signature(load_method)
         for argument, param in sig.parameters.items():
@@ -215,27 +212,28 @@ def _process_output_meta(name, bases, class_dict):
             )
             raise TypeError(msg)
 
-        if isinstance(save_method, staticmethod):
-            raise ValueError("Save method cannot be static.")
-
         sig = inspect.signature(save_method)
-        if len(sig.parameters) < 2:
-            raise TypeError("Save method requires a data parameter.")
-
-        # Ensure all arguments (except the first two, self/cls and data)
-        # have default values
-        for i, (argument, param) in enumerate(sig.parameters.items()):
-            # Skip self/cls and data
-            if i < 2:
+        has_data_argument = False
+        for idx, (argument, param) in enumerate(sig.parameters.items()):
+            if argument in {"self", "cls"}:
                 continue
+
+            has_data_argument = True
+
+            # Ensure all arguments (except the first two, self/cls and data)
+            # have default values
             if (
-                param.default is inspect.Parameter.empty
+                idx > 2
+                and param.default is inspect.Parameter.empty
                 and param.kind != inspect._ParameterKind.VAR_KEYWORD
             ):
                 raise TypeError(
                     f"Argument '{argument}' of function "
                     f"'{save_method.__name__}' has no default value."
                 )
+
+        if not has_data_argument:
+            raise TypeError("Save method requires a data parameter.")
 
         if (
             sig.return_annotation != inspect.Signature.empty

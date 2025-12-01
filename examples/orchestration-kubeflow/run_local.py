4ee813bffa3e155623e02f7deb9686755d3a28a8
Simon Brugman <sbrugman@users.noreply.github.com>
2025-12-01 13:33:45 +0100

    examples: update kubeflow example (#487)

--- examples/orchestration-kubeflow/run_local.py
+++ examples/orchestration-kubeflow/run_local.py
@@ -1,4 +1,13 @@
+from pathlib import Path
+
 import ml_pipeline
 from ordeq import run
+from ordeq_viz import viz
 
-run(ml_pipeline)
+if __name__ == "__main__":
+    run(ml_pipeline)
+    viz(
+        ml_pipeline,
+        fmt="mermaid",
+        output=Path(__file__).parent / "ml_pipeline.mermaid",
+    )

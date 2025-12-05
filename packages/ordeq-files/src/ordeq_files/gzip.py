17109ba929275e65f82226121a2e83713324939f
Niels Neerhoff <nielsneerhoff@gmail.com>
2025-12-05 12:44:02 +0100

    Avoid overloading save

--- packages/ordeq-files/src/ordeq_files/gzip.py
+++ packages/ordeq-files/src/ordeq_files/gzip.py
@@ -1,12 +1,15 @@
 import contextlib
 from dataclasses import dataclass
-from typing import Any, Literal, overload
+from typing import Any, Literal, TypeAlias, overload
 
 from ordeq import IO
 from ordeq.types import PathLike
 
 with contextlib.suppress(ImportError):
     import gzip
+
+ReadBinary: TypeAlias = Literal["r", "rb"]
+ReadText: TypeAlias = Literal["rt"]
 
 
 @dataclass(frozen=True, kw_only=True)
@@ -29,23 +32,17 @@ class Gzip(IO[bytes | str]):
     path: str | PathLike
 
     @overload
-    def load(
-        self,
-        mode: Literal["r", "rb", "w", "wb", "x", "xb", "a", "ab"] = "rb",
-        **load_options: Any,
-    ) -> bytes: ...
+    def load(self, mode: ReadBinary = "rb", **load_options: Any) -> bytes: ...
 
     @overload
-    def load(
-        self, mode: Literal["rt", "wt", "xt", "at"] = "rt", **load_options: Any
-    ) -> str: ...
+    def load(self, mode: ReadText = "rt", **load_options: Any) -> str: ...
 
-    def load(self, mode: str = "rb", **load_options: Any) -> bytes | str:
+    def load(
+        self, mode: ReadBinary | ReadText = "rb", **load_options: Any
+    ) -> bytes | str:
         with gzip.open(self.path, mode=mode, **load_options) as f:
             return f.read()
 
-    def save(
-        self, data: str | bytes, mode: str = "wb", **save_options: Any
-    ) -> None:
+    def save(self, data: bytes, mode="wb", **save_options: Any) -> None:
         with gzip.open(self.path, mode=mode, **save_options) as f:
             f.write(data)
